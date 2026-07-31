88c350d2fd24c7acde4cae5f61c1a30c55ede4af
karan07g-hue <karan.07g@gmail.com>
2026-08-01 00:59:45 +0530

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+    private void buildString(TreeNode node, StringBuilder sb){
+        if(node == null){
+            sb.append("null").append(" ,");
+        } else{
+            sb.append(node.val).append(" ,");
+            buildString(node.left, sb);
+            buildString(node.right,sb);
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> nodes = new LinkedList<>(Arrays.asList(data.split(" ,")));
+        return buildTree(nodes);
+    }
+    private TreeNode buildTree(Queue<String> nodes){
+        String val = nodes.poll();
+
+        if(val.equals("null")){
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = buildTree(nodes);
+        node.right = buildTree(nodes);
+
+        return node;
+    }
+}
